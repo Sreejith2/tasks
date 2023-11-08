02427ce149394ab17795f681f891a480062550ab
Sreejith2 <99952612+Sreejith2@users.noreply.github.com>
2023-11-08 22:25:51 +0530

    added

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
-const express = require('express')
-var hbs=require('express-handlebars')
-var path = require('path');
-const app =express();
-const port=3000;
+const express = require('express');
+const hbs = require('express-handlebars');
+const app = express();
+const port = 3000;
 
-app.set('views', path.join(__dirname, 'views'));
+
+app.set('views', __dirname + '/views');
 app.set('view engine', 'hbs');
 app.use(express.json());
 
-app.get("/",(req,res)=>{
-    const taskData = {
-        task1: "https://helloworld-p5of.onrender.com",
-        task2:"Sreejiths link-2",
-        task3:"Sreejiths link-3"
-    };
-    res.render("home",{taskData});
+app.get("/", (req, res) => {
+  const taskData = {
+    task1: "https://helloworld-p5of.onrender.com",
+    task2: "Sreejith's link-2",
+    task3: "Sreejith's link-3"
+  };
+  res.render("home", { taskData });
 });
 
-
-app.listen(3000,()=>{
-    console.log(`server started at http://localhost:${port}`);
-})
+app.listen(3000, () => {
+  console.log(`server started at http://localhost:${port}`);
+});
